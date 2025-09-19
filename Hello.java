709e367cd70f30cd5ca30be4862a0371a13eebda
Alan Roberto Lopes da Silva Silva <pet0045943@aluno.firjansenaisesi.com.br>
2025-09-18 21:33:45 -0300

    Primeira vez fazendo

---
+++ Hello.java
@@ -0,0 +1,10 @@
+public class Hello {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 10;
+        int soma = a + b;
+
+        System.out.println("A soma de " + a + " + " + b + " é: " + soma);
+
+    }ps
+}
