4949d5762835872f67caeb2c43af442ec89f82b9
Alan Roberto Lopes da Silva Silva <pet0045943@aluno.firjansenaisesi.com.br>
2025-09-24 21:30:49 -0300

    Alan Aprendendedo o bagulho meu mano

---
+++ Tabuada.java
@@ -0,0 +1,28 @@
+public class Tabuada {
+    public static void main(String[] args) {
+        int numero = 157;
+        int tabuada;
+        for(int i=1; i<=10; i++){
+            tabuada = numero * i;
+            System.out.println(numero + " x " + i + " = " + tabuada+"\n");
+        }
+        
+
+    }
+    
+
+
+
+
+
+
+}
+/*programa {
+  funcao inicio() {
+    inteiro tab, num
+    escreva("Digite um número para ver a tabuada dele: ")
+    leia(num)
+    para(inteiro c=1; c<=10; c++){
+      tab=c*num
+      escreva (num," x ", c, " = ", tab, "\n")
+    } */
