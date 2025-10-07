455955f8778f252dd941787da4ec429960c1e67e
Alan Roberto Lopes da Silva Silva <pet0045943@aluno.firjansenaisesi.com.br>
2025-10-06 21:30:05 -0300

    06/10/2025

---
+++ TimeFutebol/Jogador.java
@@ -0,0 +1,13 @@
+package TimeFutebol;
+
+public class Jogador {
+    String nome;
+    String posicao;
+
+    public void jogador(String nome,String posicao){
+        this.nome = nome;
+        this.posicao = posicao;
+    }
+} 
+    
+
